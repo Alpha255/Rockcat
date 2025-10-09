f11a16e46072f70eb895e9dd7c4c4d2aef9a2baf
alpha255 <189815810@qq.com>
2025-10-09 19:58:38 +0800

    * Refactor Shader pipeline (WIP)

--- Source/Runtime/Asset/GlobalShaders.h
+++ Source/Runtime/Asset/GlobalShaders.h
@@ -2,13 +2,25 @@
 
 #include "Assets/Shaders/Definitions.h"
 #include "Asset/Shader.h"
+#include "Paths.h"
 
 class GenericVS : public GlobalShader<GenericVS>
 {
+public:
+	GenericVS()
+		: GlobalShader(Paths::ShaderPath() / "GenericVS.vert", ERHIShaderStage::Vertex, "main")
+	{
+	}
+
 	DEFINE_SHADER_VARIABLES_GENERIC_VS;
 };
 
 class DepthOnlyFS : public GlobalShader<DepthOnlyFS>
 {
+	DepthOnlyFS()
+		: GlobalShader(Paths::ShaderPath() / "DepthOnly.frag", ERHIShaderStage::Fragment, "main")
+	{
+	}
+
 	DEFINE_SHADER_VARIABLES_DEPTH_ONLY_FS;
 };
