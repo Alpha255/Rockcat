4ac1c0d6359c601612a7fb67d9a4ba778a01e0b5
alpha255 <189815810@qq.com>
2026-02-06 17:16:51 +0800

    * Reconstruct task class, code compiled (WIP)
    * Sync tasks cross executors like this ???

--- Source/Runtime/Async/Task.cpp
+++ Source/Runtime/Async/Task.cpp
@@ -1,7 +1,7 @@
 #include "Async/Task.h"
 #include "Core/ConsoleVariable.h"
 #include "System/System.h"
-#include "Services/TaskFlowService.h"
+#include "Services/SpdLogService.h"
 
 thread_local TFTask::EThread t_ThreadTag = TFTask::EThread::WorkerThread;
 
@@ -47,17 +47,142 @@ struct ThreadPriorityScope
 	bool SkipPriorityChange;
 };
 
+class TFExecutorManager : public Singleton<TFExecutorManager>
+{
+public:
+	void Initialize()
+	{
+		std::vector<size_t> WorkersInExecutor
+		{
+			CVarUseSeperateGameThread.Get(),
+			CVarUseSeperateRenderThread.Get(),
+			CVarNumForegroundThreads.Get(),
+			TFTask::GetNumWorkerThreads()
+		};
+
+		m_Executors.reserve(WorkersInExecutor.size());
+
+		for (auto& NumWorkers : WorkersInExecutor)
+		{
+			if (NumWorkers > 0u)
+			{
+				m_Executors.emplace_back(std::make_unique<tf::Executor>(NumWorkers));
+			}
+		}
+
+		if (CVarNumForegroundThreads.Get())
+		{
+			auto Executor = GetExecutor(TFTask::EThread::WorkerThread, TFTask::EPriority::High);
+			assert(Executor);
+
+			tf::Taskflow Flow;
+			std::vector<tf::Taskflow> SubFlows(CVarNumForegroundThreads.Get());
+
+			for (auto& SubFlow : SubFlows)
+			{
+				SubFlow.emplace([]() {
+					System::SetThreadPriority(std::this_thread::get_id(), TFTask::EPriority::High);
+
+					std::stringstream Stream;
+					Stream << std::this_thread::get_id();
+					uint32_t ThreadID = std::stoul(Stream.str());
+					LOG_INFO_CAT(LogTaskFlow, "Set foreground thread {} high priority", ThreadID);
+				});
+			}
+			for (uint32_t Index = 0u; Index < SubFlows.size(); ++Index)
+			{
+				Flow.emplace([Executor, &SubFlow = SubFlows[Index]]() {
+					Executor->corun(SubFlow);
+				});
+			}
+			Executor->run(Flow).wait();
+		}
+
+		const uint32_t NumSeperateThreads = CVarUseSeperateGameThread.Get() + CVarUseSeperateRenderThread.Get() + CVarNumForegroundThreads.Get() > 0u;
+		LOG_INFO_CAT(LogTaskFlow, "Create executors with {} seperate threads, {} worker threads, hyper threading is {}",
+			NumSeperateThreads,
+			TFTask::GetNumWorkerThreads(),
+			CVarUseHyperThreading.Get() ? "enabled" : "disabled");
+	}
+
+	void Finalize()
+	{
+		for (auto& Executor : m_Executors)
+		{
+			if (Executor)
+			{
+				Executor->wait_for_all();
+				Executor.reset();
+			}
+		}
+	}
+
+	tf::Executor* GetExecutor(TFTask::EThread Thread, TFTask::EPriority Priority)
+	{
+		assert(Thread < TFTask::EThread::Num);
+
+		if (!CVarUseSeperateGameThread.Get() && Thread == TFTask::EThread::GameThread)
+		{
+			return nullptr;
+		}
+
+		if (!CVarUseSeperateRenderThread.Get() && Thread == TFTask::EThread::RenderThread)
+		{
+			return nullptr;
+		}
+
+		size_t Index = static_cast<size_t>(Thread);
+		if (Thread == TFTask::EThread::WorkerThread && Priority > TFTask::EPriority::Normal && CVarNumForegroundThreads.Get())
+		{
+			Index += 1u;
+		}
+
+		return m_Executors[Index].get();
+	}
+private:
+	std::vector<std::unique_ptr<tf::Executor>> m_Executors;
+};
+
+void TFTask::Initialize()
+{
+	TFExecutorManager::Get().Initialize();
+	InitializeThreadTags();
+
+	LOG_INFO_CAT(LogTaskFlow, "Use taskflow @{}", tf::version());
+}
+
+void TFTask::Finalize()
+{
+	TFExecutorManager::Get().Finalize();
+}
+
 void TFTask::InitializeThreadTags()
 {
 	t_ThreadTag = TFTask::EThread::MainThread;
 
-	tf::Async_WaitDone([]() {
-		t_ThreadTag = TFTask::EThread::GameThread;
-	}, TFTask::EThread::GameThread);
+	if (CVarUseSeperateGameThread.Get())
+	{
+		Launch("tf.SetGameThreadTag", []() {
+			t_ThreadTag = TFTask::EThread::GameThread;
+		}, EThread::GameThread);
+	}
 
-	tf::Async_WaitDone([]() {
-		t_ThreadTag = TFTask::EThread::RenderThread;
-	}, EThread::RenderThread);
+	if (CVarUseSeperateRenderThread.Get())
+	{
+		Launch("tf.SetRenderThreadTag", []() {
+			t_ThreadTag = TFTask::EThread::RenderThread;
+		}, EThread::RenderThread);
+	}
+}
+
+TFTaskEventPtr TFTask::DispatchTaskFlow(tf::Taskflow&& Flow, EThread Thread, EPriority Priority)
+{
+	if (auto Executor = TFExecutorManager::Get().GetExecutor(Thread, Priority))
+	{
+		return std::make_shared<TFTaskEvent>(std::move(Executor->run(std::forward<tf::Taskflow>(Flow))));
+	}
+
+	return nullptr;
 }
 
 uint32_t TFTask::GetNumWorkerThreads()
@@ -98,68 +223,6 @@ bool TFTask::IsWorkerThread()
 {
 	return t_ThreadTag == EThread::WorkerThread;
 }
-
-class TFExecutorManager : public Singleton<TFExecutorManager>
-{
-public:
-	void Initialize()
-	{
-		std::vector<size_t> WorkersInExecutor
-		{
-			CVarUseSeperateGameThread.Get(),
-			CVarUseSeperateRenderThread.Get(),
-			CVarNumForegroundThreads.Get(),
-			TFTask::GetNumWorkerThreads()
-		};
-
-		m_Executors.reserve(WorkersInExecutor.size());
-
-		for (auto& NumWorkers : WorkersInExecutor)
-		{
-			if (NumWorkers > 0u)
-			{
-				m_Executors.emplace_back(std::make_unique<tf::Executor>(NumWorkers));
-			}
-		}
-	}
-
-	void Finalize()
-	{
-		for (auto& Executor : m_Executors)
-		{
-			if (Executor)
-			{
-				Executor->wait_for_all();
-				Executor.reset();
-			}
-		}
-	}
-
-	tf::Executor* GetExecutor(TFTask::EThread Thread, TFTask::EPriority Priority)
-	{
-		assert(Thread < TFTask::EThread::Num);
-
-		if (!CVarUseSeperateGameThread.Get() && Thread == TFTask::EThread::GameThread)
-		{
-			return nullptr;
-		}
-
-		if (!CVarUseSeperateRenderThread.Get() && Thread == TFTask::EThread::RenderThread)
-		{
-			return nullptr;
-		}
-
-		size_t Index = static_cast<size_t>(Thread);
-		if (Thread == TFTask::EThread::WorkerThread && Priority > TFTask::EPriority::Normal && CVarNumForegroundThreads.Get())
-		{
-			Index += 1u;
-		}
-
-		return m_Executors[Index].get();
-	}
-private:
-	std::vector<std::unique_ptr<tf::Executor>> m_Executors;
-};
 
 void TFTask::AddPrerequisite(TFTask& Prerequisite)
 {
@@ -208,7 +271,7 @@ void TFTask::Trigger()
 		return;
 	}
 
-	if (auto Executor = TaskFlow::Get().GetExecutor(m_Thread, m_Priority))
+	if (auto Executor = TFExecutorManager::Get().GetExecutor(m_Thread, m_Priority))
 	{
 		std::vector<tf::AsyncTask> PrerequisiteTasks;
 		PrerequisiteTasks.reserve(m_Prerequisites.size());
@@ -226,7 +289,7 @@ void TFTask::Trigger()
 			}
 		}
 
-		m_AsyncTask = std::make_shared<tf::AsyncTask>(std::move(Executor->dependent_async([this]() {
+		m_TFAsyncTask = std::make_shared<TFAsyncTask>(std::move(Executor->dependent_async([this]() {
 			ThreadPriorityScope ScopedTaskPriority(m_Priority);
 			Execute();
 			TriggerSubsequents();
@@ -244,3 +307,37 @@ bool TFTask::TryCancel()
 	SetCanceled(true);
 	return true;
 }
+
+//void TaskFlow::FrameSync(bool AllowOneFrameLag)
+//{
+//	if (m_SeparateRenderThread)
+//	{
+//		struct FrameSyncTask : public Task
+//		{
+//		protected:
+//			virtual void ExecuteImpl() {}
+//		};
+//		static FrameSyncTask s_FrameSyncTask;
+//
+//		m_ThreadSyncEvents[m_ThreadEventIndex] = DispatchTask(s_FrameSyncTask, EThread::RenderThread);
+//
+//		if (AllowOneFrameLag)
+//		{
+//			m_ThreadEventIndex = (m_ThreadEventIndex + 1u) % m_ThreadSyncEvents.size();
+//		}
+//
+//		if (m_ThreadSyncEvents[m_ThreadEventIndex])
+//		{
+//			if (m_SeparateGameThread)
+//			{
+//				auto GameThreadCompledEvent = DispatchTask(s_FrameSyncTask, EThread::GameThread);
+//				do {
+//					GameThreadCompledEvent->WaitForMilliseconds(1u);
+//				} while (!GameThreadCompledEvent->IsCompleted());
+//			}
+//
+//			m_ThreadSyncEvents[m_ThreadEventIndex]->Wait();
+//			m_ThreadSyncEvents[m_ThreadEventIndex].reset();
+//		}
+//	}
+//}
--- Source/Runtime/Async/Task.h
+++ Source/Runtime/Async/Task.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include "Core/Definitions.h"
 #include "Core/Name.h"
+#include "Core/Singleton.h"
 
 #pragma warning(push)
 #pragma warning(disable:4456 4244 4127 4267 4324)
+#include <taskflow/taskflow/utility/traits.hpp>
 #include <taskflow/taskflow.hpp>
 #include <taskflow/core/task.hpp>
 #include <taskflow/algorithm/for_each.hpp>
@@ -94,8 +95,7 @@ public:
 		: m_Thread(Thread)
 		, m_Priority(Priority)
 		, m_Name(std::move(Name))
-		, m_TaskFunc([Func=std::forward<LAMBDA>(Lambda)]() {
-			Func() })
+		, m_TaskFunc(std::move([Func = std::forward<LAMBDA>(Lambda)]() { Func(); }))
 	{
 	}
 
@@ -109,8 +109,8 @@ public:
 
 	~TFTask() = default;
 
-	inline bool IsCompleted() const { return m_AsyncTask ? m_AsyncTask->is_done() : false; }
-	inline bool IsDispatched() const { return m_Event ? true : false; }
+	inline bool IsCompleted() const { return m_TFAsyncTask ? m_TFAsyncTask->first.is_done() : false; }
+	inline bool IsDispatched() const { return m_TFAsyncTask ? true : false; }
 	inline bool IsCanceled() const { return m_Canceled.load(std::memory_order_acquire); }
 
 	inline const FName& GetName() const { return m_Name; }
@@ -118,6 +118,33 @@ public:
 	void AddPrerequisite(TFTask& Prerequisite);
 
 	void Trigger();
+
+	inline void Wait() 
+	{
+		if (m_TFAsyncTask)
+		{
+			m_TFAsyncTask->second.wait();
+		}
+	}
+
+	inline void WaitForSeconds(size_t Seconds) 
+	{
+		if (m_TFAsyncTask)
+		{
+			m_TFAsyncTask->second.wait_for(std::chrono::seconds(Seconds));
+		}
+	}
+
+	inline void WaitForMilliseconds(size_t Milliseconds)
+	{
+		if (m_TFAsyncTask)
+		{
+			m_TFAsyncTask->second.wait_for(std::chrono::milliseconds(Milliseconds));
+		}
+	}
+
+	static void Initialize();
+	static void Finalize();
 
 	static bool IsMainThread();
 	static bool IsGameThread();
@@ -127,20 +154,49 @@ public:
 	static uint32_t GetNumWorkerThreads();
 
 	template<class LAMBDA>
-	static std::shared_ptr<TFTask> TryLaunch(FName&& Name, LAMBDA&& Lambda, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
+	static std::shared_ptr<TFTask> Launch(FName&& Name, LAMBDA&& Lambda, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
 	{
 		auto Task = std::make_shared<TFTask>(std::forward<FName>(Name), std::forward<LAMBDA>(Lambda), Thread, Priority);
 		Task->Trigger();
 		return Task;
 	}
 
-	template<class LAMBDA, class Prerequisites>
-	static std::shared_ptr<TFTask> TryLaunch(FName&& Name, LAMBDA&& Lambda, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
+	template<class LAMBDA>
+	static std::shared_ptr<TFTask> Launch(FName&& Name, LAMBDA&& Lambda, std::vector<TFTask*>&& PrerequisiteTasks, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
 	{
+		auto Task = std::make_shared<TFTask>(std::forward<FName>(Name), std::forward<LAMBDA>(Lambda), Thread, Priority);
+		for (auto PrerequisiteTask : PrerequisiteTasks)
+		{
+			Task->AddPrerequisite(*PrerequisiteTask);
+		}
 
+		Task->Trigger();
+		return Task;
+	}
+
+	template<class Iterator, class LAMBDA>
+	static TFTaskEventPtr ParallelFor(Iterator&& Begin, Iterator&& End, LAMBDA&& Lambda, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
+	{
+		assert(Thread < EThread::Num);
+
+		tf::Taskflow TFTaskFlow;
+		TFTaskFlow.for_each(std::forward<Iterator>(Begin), std::forward<Iterator>(End), std::forward<LAMBDA>(Lambda));
+		return DispatchTaskFlow(std::move(TFTaskFlow), Thread, Priority);
+	}
+
+	template<class Iterator, class LAMBDA>
+	static TFTaskEventPtr ParallelSort(Iterator&& Begin, Iterator&& End, LAMBDA&& Lambda, EThread Thread = EThread::WorkerThread, EPriority Priority = EPriority::Normal)
+	{
+		assert(Thread < EThread::Num);
+
+		tf::Taskflow TFTaskFlow;
+		TFTaskFlow.sort(std::forward<Iterator>(Begin), std::forward<Iterator>(End), std::forward<LAMBDA>(Lambda));
+		return DispatchTaskFlow(std::move(TFTaskFlow), Thread, Priority);
 	}
 protected:
 	static void InitializeThreadTags();
+
+	static TFTaskEventPtr DispatchTaskFlow(tf::Taskflow&&, EThread Thread, EPriority Priority);
 
 	void AddSubsequents(TFTask& Subsequent);
 
@@ -148,7 +204,7 @@ protected:
 
 	bool TryCancel();
 
-	inline tf::AsyncTask* GetAsyncTask() { return m_AsyncTask.get(); }
+	inline tf::AsyncTask* GetAsyncTask() { return &m_TFAsyncTask->first; }
 
 	inline void SetCanceled(bool Canceled) { m_Canceled.store(Canceled, std::memory_order_release); }
 
@@ -164,6 +220,8 @@ protected:
 		return m_NumRef.fetch_sub(1u, std::memory_order_acq_rel) == 1u;
 	}
 private:
+	using TFAsyncTask = std::pair<tf::AsyncTask, std::future<void>>;
+
 	EThread m_Thread = EThread::WorkerThread;
 	EPriority m_Priority = EPriority::Normal;
 
@@ -179,6 +237,5 @@ private:
 
 	std::function<void()> m_TaskFunc;
 
-	std::shared_ptr<tf::AsyncTask> m_AsyncTask;
-	std::shared_ptr<TFTaskEvent> m_Event;
+	std::shared_ptr<TFAsyncTask> m_TFAsyncTask;
 };
